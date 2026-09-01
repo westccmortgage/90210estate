b1da7ca1d98c7ed3f06eb4dd3f4ed4f3cd15cfe4
Anatoliy Kanevsky <westccmortgage@gmail.com>
2026-08-31 20:56:23 -0700

    Strengthen robots and sitemap discovery

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,12 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/"],
+    },
     sitemap: "https://90210estate.com/sitemap.xml",
+    host: "https://90210estate.com",
   };
 }
